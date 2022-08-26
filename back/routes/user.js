2b3ef53b5320fb48527dbada6801bd96e66595cc
LionelVanS <lionelvs@outlook.fr>
2022-08-26 16:49:11 +0200

    Ajout du package express rate limit

--- back/routes/user.js
+++ back/routes/user.js
@@ -2,11 +2,14 @@
 const express = require('express');
 const router = express.Router();
 
+// Appel de limite d'essai de connexion
+const rateLimit = require('../middleware/rateLimit')
+
 // Appel du contrôleur utilisateur
 const userCtrl = require('../controllers/user');
 
 // Déclaration des routes utilisateur
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', rateLimit, userCtrl.login);
 
 module.exports = router;
